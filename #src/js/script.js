2ab3441bd15bca4abed38fc26aaec6e9cd6fd0b1
butterbroad <pyatochevskiy@gmail.com>
2020-11-03 18:26:10 +0300

    initial commit

---
+++ #src/js/script.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+const langTrigger = document.querySelector('.lang__items');
+const langList = document.querySelector('.lang__item-list');
+langTrigger.addEventListener('click', () => {
+  langList.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+@@include('webpSupport.js')
---
+++ football-affiliate/js/script.js
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+const langTrigger = document.querySelector('.lang__items');
+const langList = document.querySelector('.lang__item-list');
+langTrigger.addEventListener('click', () => {
+  langList.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+function testWebP(callback) {
+
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    } else {
+        document.querySelector('body').classList.add('no-webp');
+    }
+});
