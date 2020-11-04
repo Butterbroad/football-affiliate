7db7b6fdfce1f01b82b30cb0de06441971650565
butterbroad <pyatochevskiy@gmail.com>
2020-11-04 15:44:59 +0300

    finished desktop markup

--- football-affiliate/js/script.js
+++ football-affiliate/js/script.js
@@ -1,19 +1,48 @@
+//register links
+const registerLinks = document.querySelectorAll('.get-link');
 
+if (registerLinks.length > 0) {
+  registerLinks.forEach(item => {
+    item.setAttribute('href', item.getAttribute('href') + window.location.search);
+  });
+}
 
-
-
-
-
+//language
 const langTrigger = document.querySelector('.lang__items');
 const langList = document.querySelector('.lang__item-list');
 langTrigger.addEventListener('click', () => {
   langList.classList.toggle('active');
 });
 
+//tabs
+const tabTriggers = document.querySelectorAll('.tabs__trigger');
+const tabItems = document.querySelectorAll('.tabs__item');
 
 
+tabTriggers.forEach(trigger => {
+  trigger.addEventListener('click', (e) => {
+    tabItems.forEach(item => {
+      if (item.classList.contains('active')) {
+        item.classList.remove('active');
+      }
+    })
+    const parent = e.target.parentElement;
+    parent.classList.add('active');
+  });
+});
 
 
+//slider
+const swiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  slidesPerView: 1,
+  spaceBetween: 30,
+  mousewheel: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
 
 
 
