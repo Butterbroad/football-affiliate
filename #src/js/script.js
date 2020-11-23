db9e18d8b21803d0681abcdc491d7f062f0f9617
butterbroad <pyatochevskiy@gmail.com>
2020-11-23 18:05:02 +0300

    started multilanguage

--- #src/js/script.js
+++ #src/js/script.js
@@ -7,7 +7,7 @@ if (registerLinks.length > 0) {
   });
 }
 
-//language
+//language toggler
 const langTrigger = document.querySelector('.lang__items');
 const langList = document.querySelector('.lang__item-list');
 langTrigger.addEventListener('click', () => {
@@ -33,7 +33,8 @@ tabTriggers.forEach(trigger => {
 
 
 //slider
-var menu = ['СЕРИЯ БОНУСОВ', 'БОНУС 100%', '1000$ ЗА ПРОГНОЗ', 'ML ПЛАТИТ/ВЕРНЕТ', '4% CASHBACK', 'УСЛОВИЯ']
+const menu = ['СЕРИЯ БОНУСОВ', 'БОНУС 100%', '1000$ ЗА ПРОГНОЗ', 'ML ПЛАТИТ/ВЕРНЕТ', '4% CASHBACK', 'УСЛОВИЯ']
+const classNames = ['navigationLinkOne', 'navigationLinkTwo', 'navigationLinkThree', 'navigationLinkFour', 'navigationLinkFive', 'navigationLinkSix'];
 const swiper = new Swiper('.swiper-container', {
   direction: 'horizontal',
   slidesPerView: 1,
@@ -47,7 +48,7 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination',
     clickable: true,
     renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (menu[index]) + '</span>';
+      return `<span class="${className} ${classNames[index]}">${menu[index]}</span>`;
     },
   },
 });
@@ -58,6 +59,90 @@ scrollableBlocks.forEach(elem => {
     e.stopPropagation();
   });
 })
+
+const changeSlideHeight = document.querySelector('.slide-six');
+swiper.on('transitionStart', () => {
+  if (swiper.isEnd) {
+    if (window.innerWidth < 426) {
+      changeSlideHeight.style.height = 'auto';
+    }
+  } else {
+    if (window.innerWidth < 426) {
+      changeSlideHeight.style.height = '75vh';
+    }
+  }
+});
+
+//mobile menu
+const burger = document.querySelector('.header__burger');
+const mobileMenu = document.querySelector('.swiper-pagination__wrapper');
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
+});
+
+
+
+//multilanguage
+@@include('translate.js');
+
+const langItems = document.querySelectorAll('.lang__item');
+
+//main Btn
+const mainBtn = document.querySelector('.btn_main');
+
+//navigation
+const navigationLinkOne = document.querySelector('.navigationLinkOne');
+const navigationLinkTwo = document.querySelector('.navigationLinkTwo');
+const navigationLinkThree = document.querySelector('.navigationLinkThree');
+const navigationLinkFour = document.querySelector('.navigationLinkFour');
+const navigationLinkFive = document.querySelector('.navigationLinkFive');
+const navigationLinkSix = document.querySelector('.navigationLinkSix');
+
+//slide 1
+const textLine1 = document.querySelector('.slide-one__text-line_1');
+const textLine2 = document.querySelector('.slide-one__text-line_2');
+const textLine3 = document.querySelector('.slide-one__text-line_3');
+
+
+
+
+
+
+langItems.forEach(item => {
+  item.addEventListener('click', () => {
+    const attr = item.getAttribute('language');
+
+    //btn
+    mainBtn.textContent = translate[attr].mainBtn;
+
+    //navigation
+    navigationLinkOne.textContent = translate[attr].navigationLinkOne;
+    navigationLinkTwo.textContent = translate[attr].navigationLinkTwo;
+    navigationLinkThree.textContent = translate[attr].navigationLinkThree;
+    navigationLinkFour.textContent = translate[attr].navigationLinkFour;
+    navigationLinkFive.textContent = translate[attr].navigationLinkFive;
+    navigationLinkSix.textContent = translate[attr].navigationLinkSix;
+
+    //slide one
+    textLine1.textContent = translate[attr].slideOneTextLineOne;
+    textLine2.textContent = translate[attr].slideOneTextLineTwo;
+    textLine3.textContent = translate[attr].slideOneTextLineThree;
+
+
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 @@include('webpSupport.js')
---
+++ #src/js/translate.js
@@ -0,0 +1,26 @@
+const translate = {
+  "russian": {
+    "mainBtn": "РЕГИСТРАЦИЯ",
+    'navigationLinkOne': 'СЕРИЯ БОНУСОВ',
+    'navigationLinkTwo': 'БОНУС 100%',
+    'navigationLinkThree': '1000$ ЗА ПРОГНОЗ',
+    'navigationLinkFour': 'ML ПЛАТИТ/ВЕРНЕТ',
+    'navigationLinkFive': '4% CASHBACK',
+    'navigationLinkSix': 'УСЛОВИЯ',
+    "slideOneTextLineOne": "БОЛЬШАЯ",
+    "slideOneTextLineTwo": "СЕРИЯ БОНУСОВ",
+    "slideOneTextLineThree": "Больше игры - больше бонусов",
+  },
+  "english": {
+    "mainBtn": "REGISTRATION",
+    'navigationLinkOne': 'SERIES OF BONUSES',
+    'navigationLinkTwo': 'BONUS 100%',
+    'navigationLinkThree': '1000 $ PER FORECAST',
+    'navigationLinkFour': 'ML PAYS / RETURNS',
+    'navigationLinkFive': '4% CASHBACK',
+    'navigationLinkSix': 'CONDITIONS',
+    "slideOneTextLineOne": "BIG",
+    "slideOneTextLineTwo": "SERIES OF BONUSES",
+    "slideOneTextLineThree": "More game - more bonuses",
+  }
+}
